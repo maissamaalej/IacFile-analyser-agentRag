11146765289afa1d21e9c0f25300bb32c0e8a40d
maissa <131200482+maissamaalej@users.noreply.github.com>
2026-09-07 16:47:20 +0100

    Update model.py

--- src/main/java/org/example/agentrag/agentic-rag-python/app/services/model.py
+++ src/main/java/org/example/agentrag/agentic-rag-python/app/services/model.py
@@ -1,38 +1,34 @@
+import os
+
 from sentence_transformers import SentenceTransformer
 
 
 class EmbeddingModel:
 
-
     def __init__(self):
 
-        self.model = SentenceTransformer(
+        model_name = os.getenv(
+            "EMBEDDING_MODEL",
             "BAAI/bge-small-en-v1.5"
         )
 
-
-
-    def encode(
-            self,
-            texts: list[str]
-    ):
-
-
-        embeddings = self.model.encode(
-
-            texts,
-
-            batch_size=64,
-
-            normalize_embeddings=True,
-
-            show_progress_bar=False
-
+        self.model = SentenceTransformer(
+            model_name
         )
 
+    def encode(
+        self,
+        texts: list[str]
+    ):
+
+        embeddings = self.model.encode(
+            texts,
+            batch_size=64,
+            normalize_embeddings=True,
+            show_progress_bar=False
+        )
 
         return embeddings.tolist()
-
 
 
 embedding_model = EmbeddingModel()
